be20d9e56205016f6f0b13c6401dabe1925ad66c
Avaneesh Shetti <shettiavaneesh@gmail.com>
2026-09-08 10:20:04 -0700

    Renamed Variabkes

--- NumberRiddle.java
+++ NumberRiddle.java
@@ -1,12 +1,12 @@
 public class NumberRiddle {
     public static void main(String[] args) {
-        int startingNumberYouChoose = 20;
-        int intPosTestCase = 2;
-        int intNegTestCase = -2;
+        int startingNumber = 20;
+        int intPos = 2;
+        int intNeg = -2;
         int zeroTest = 0;
         int oneTest = 1;
-        double posDoubleTest = 6.7;
-        double negDoubleTest = -6.7;
-        
+        double posDouble = 6.7;
+        double negDouble = -6.7;
+
     }
 }
