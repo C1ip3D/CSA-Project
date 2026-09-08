cccc58d4bf1bafbb247cb4e4beed841cd9fb870e
Avaneesh Shetti <shettiavaneesh@gmail.com>
2026-09-08 10:28:28 -0700

    Algorithm

--- NumberRiddle.java
+++ NumberRiddle.java
@@ -2,11 +2,44 @@ public class NumberRiddle {
     public static void main(String[] args) {
         int startingNumber = 20;
         int intPos = 2;
+        int intPosCopy = 2;
         int intNeg = -2;
+        int intNegCopy = -2;
         int zeroTest = 0;
+        int zeroTestCopy = 0;
         int oneTest = 1;
+        int oneTestCopy = 1;
         double posDouble = 6.7;
+        double posDoubleCopy = 6.7;
         double negDouble = -6.7;
+        double negDoubleCopy = -6.7;
 
+        intPos *= 2;
+        intNeg *= 2;
+        zeroTest *= 2;
+        oneTest *= 2;
+        posDouble *= 2;
+        negDouble *= 2;
+
+        intPos += 6;
+        intNeg += 6;
+        zeroTest += 6;
+        oneTest += 6;
+        posDouble += 6;
+        negDouble += 6;
+
+        intPos /= 2;
+        intNeg /= 2;
+        zeroTest /= 2;
+        oneTest /= 2;
+        posDouble /= 2;
+        negDouble /= 2;
+
+        intPos -= intPosCopy;
+        intNeg -= intNegCopy;
+        zeroTest -= zeroTestCopy;
+        oneTest -= oneTestCopy;
+        posDouble -= posDoubleCopy;
+        negDouble -= negDoubleCopy;
     }
 }
